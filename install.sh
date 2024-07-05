d02136aa87ff30321b4a36c681663d43a9e76303
Francisco Noriega <frannoriega.92@gmail.com>
2024-07-05 09:19:33 -0300

    script: Improved script to autoinstall

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ fi
 
 # Install essentials
 # Make sure git is installed
-pacman -S --needed git base-devel
+pacman -S --noconfirm --needed git base-devel
 
 ## YAY
 git clone https://aur.archlinux.org/yay-bin.git
@@ -15,7 +15,7 @@ popd
 rm -rf yay-bin
 
 ## GNU Stow
-yay -S stow
+yay -Syu --noconfirm stow
 
 ## dotfiles
 git clone https://github.com/frannoriega/dotfiles.git
@@ -29,18 +29,35 @@ stow rofi -t ~
 
 
 ## Nerd fonts
-yay -S ttf-hack-nerd
+yay -Syu --noconfirm ttf-hack-nerd
 
 ## Picom
-sudo pacman -S libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev meson ninja-build uthash-dev
-yay -S picom
+sudo pacman -Syu --noconfirm libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev meson ninja-build uthash-dev
+yay -Syu --noconfirm picom
 
 ## WezTerm
-yay -S wezterm
+yay -Syu --noconfirm wezterm
+
+## Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+## Zellij
+
+
+## Starship
+curl -sS https://starship.rs/install.sh | sh
 
 ## Audio control
-yay -S pavucontrol
+yay -Syu --noconfirm pavucontrol
+
+# Install developing essentials
+## Docker
+yay -Syu --noconfirm docker
+systemctl enable docker
+systemctl start docker
+
+## 
 
 # Install applications
 ## Brave
-yay -S brave
+yay -Syu --noconfirm brave
