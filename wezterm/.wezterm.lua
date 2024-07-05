a706a03f55a858494744a4859e665bd4d0c400fc
Francisco Noriega <frannoriega.92@gmail.com>
2024-07-05 09:19:36 -0300

    wezterm: Added font size for MacOS

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,3 +1,14 @@
+-- Function to detect the platform
+function os.capture(cmd, raw)
+  local f = assert(io.popen(cmd, 'r'))
+  local s = assert(f:read('*a'))
+  f:close()
+  if raw then return s end
+  s = string.gsub(s, '^%s+', '')
+  s = string.gsub(s, '%s+$', '')
+  s = string.gsub(s, '[\n\r]+', ' ')
+  return s
+end
 
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
@@ -12,6 +23,11 @@ config.color_scheme = 'AdventureTime'
 
 -- Set nerd font
 config.font = wezterm.font 'Hack Nerd Font'
+
+-- Set font size for MacOS
+if os.capture('uname') == "Darwin" then
+  config.font_size = 18.0
+end
 
 -- Set catppuccin
 config.color_scheme = 'catppuccin-mocha'
